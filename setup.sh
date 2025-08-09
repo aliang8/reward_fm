f830ea052eb9458b43ec3b45507dfc108fd411c0
Anthony Liang <aliang80@usc.edu>
2025-08-09 19:21:07 +0000

    updating

---
+++ setup.sh
@@ -0,0 +1,5 @@
+mkdir rfm_dataset
+cd rfm_dataset
+git clone https://huggingface.co/datasets/abraranwar/agibotworld_rfm
+git clone https://huggingface.co/datasets/abraranwar/libero_rfm
+git clone https://huggingface.co/datasets/abraranwar/egodex_rfm

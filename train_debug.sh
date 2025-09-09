31635215dadda5d26b03f04eabbeaa391d5b98c1
Anthony Liang <aliang80@usc.edu>
2025-09-09 21:26:05 +0000

    merge with main

--- train_debug.sh
+++ train_debug.sh
@@ -6,7 +6,7 @@ export RFM_DATASET_PATH=/home/thecodeboy/reward_fm/rfm_dataset
 
 # Run training with FSDP using accelerate launch
 uv run accelerate launch \
-    --config_file rfm/configs/fsdp_debug.yaml \
+    --config_file rfm/configs/fsdp_single.yaml \
     train.py \
     --config rfm/configs/config.yaml \
     --data.resized_height 128 \
@@ -15,6 +15,4 @@ uv run accelerate launch \
     --debug true \
     --model.train_preference_head true \
     --model.train_progress_head true \
-    --training.output_dir ./logs/rfm_debug 
-    
-    # --model.base_model_id Qwen/Qwen2.5-VL-7B-Instruc\B
+    --training.output_dir ./logs/rfm_debug

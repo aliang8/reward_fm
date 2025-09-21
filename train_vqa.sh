23f987cf8a6f97e9c0fb83bd3b9d9e98eb178b5e
Anthony Liang <aliangdw@umich.edu>
2025-09-21 19:09:22 +0000

    updates

--- train_vqa.sh
+++ train_vqa.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 
-# Script to run RFM training with FSDP using accelerate launch
-export RFM_DATASET_PATH=/home/thecodeboy/reward_fm/rfm_dataset
+export RFM_DATASET_PATH=/scr/shared/reward_fm/rfm_dataset
+export RFM_PROCESSED_DATASETS_PATH=/scr/shared/reward_fm/processed_datasets
 
 # Run training with FSDP using accelerate launch
-CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 uv run accelerate launch \
-    --config_file rfm/configs/fsdp.yaml \
+CUDA_VISIBLE_DEVICES=6 uv run accelerate launch \
+    --config_file rfm/configs/fsdp_single.yaml \
     train.py \
     --config_paths rfm/configs/config.yaml rfm/configs/vqa_config.yaml \
-    --data.resized_height 128 \
-    --data.resized_width 128 \
     --logging.use_wandb true \
     --debug false \
     --training.output_dir ./logs/rfm_vqa

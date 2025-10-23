4580908cab4947b25a11d787da2affa8e59cff8d
Jesse Zhang <jessezhang5@gmail.com>
2025-10-23 15:24:45 -0700

    update

--- dataset_upload/dataset_loaders/motif_loader.py
+++ dataset_upload/dataset_loaders/motif_loader.py
@@ -108,10 +108,9 @@ def load_motif_dataset(dataset_path: str) -> dict[str, list[dict]]:
 
     # Annotations
     ann_dir = root / "annotations"
-    human_json = json.load(open(ann_dir / "human_motion_data_info.json"))
-    stretch_json = json.load(open(ann_dir / "stretch_motion_data_info.json"))
-    for json in [human_json, stretch_json]:
-        path_precursor = "human_motion/videos_raw" if "human" in json else "stretch_motion/videos_raw"
+    for json_file_name in ["human_motion_data_info.json", "stretch_motion_data_info.json"]:
+        path_precursor = "human_motion/videos_raw" if "human" in json_file_name.split("_")[0] else "stretch_motion/videos_raw"
+        json = json.load(open(ann_dir / json_file_name))
         for item in json:
             src = item["video_path"].split("/")[-1]
             full_vid_path = root / path_precursor / src

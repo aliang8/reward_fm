4a7a3a616c334280fa1dfb4e426b6eea8bdd2a57
Jesse Zhang <jessezha@cs.washington.edu>
2025-10-27 20:34:14 -0700

    added egodex download and cnvert stcript

---
+++ dataset_upload/data_scripts/egodex/download_and_convert.sh
@@ -0,0 +1,30 @@
+for part in test part1 part2 part3 part4 part5; do
+
+    echo "Processing ${part}..."
+
+    # Download the dataset. 
+    # Example curl "https://ml-site.cdn-apple.com/datasets/egodex/test.zip" -o test.zip
+    # Download it to ~/egodex/${part}.zip
+    echo "curl "https://ml-site.cdn-apple.com/datasets/egodex/${part}.zip" -o ${HOME}/egodex/${part}.zip"
+    curl "https://ml-site.cdn-apple.com/datasets/egodex/${part}.zip" -o ${HOME}/egodex/${part}.zip
+    unzip -d ${HOME}/egodex/${part} ${HOME}/egodex/${part}.zip
+
+    rm ${HOME}/egodex/${part}.zip
+
+
+    uv run python -m dataset_upload.generate_hf_dataset \
+        --config_path=dataset_upload/configs/data_gen_configs/egodex.yaml \
+        --dataset.dataset_path="${HOME}/egodex/${part}/${part}" \
+        --dataset.dataset_name="egodex_${part}" \
+        --output.output_dir=./rfm_dataset/egodex_rfm \
+        --hub.push_to_hub=false
+
+    echo "Done processing ${part}..."
+
+    # Delete the dataset
+    echo "Deleting ${HOME}/egodex/${part}..."
+    exit 1
+    rm -rf ${HOME}/egodex/${part}
+    echo "Done deleting ${HOME}/egodex/${part}..."
+
+done

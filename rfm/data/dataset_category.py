fa864de83d545f4f8d51c59b9b5b0aba4bc21e2f
Jesse Zhang <jessezha@cs.washington.edu>
2025-11-24 09:41:51 -0800

    added egodex to reward alignment

--- rfm/data/dataset_category.py
+++ rfm/data/dataset_category.py
@@ -129,7 +129,7 @@ DATASET_MAP = {
         ],
     },
     "reward_alignment": {
-        "eval": ["aliangdw_metaworld_metaworld_eval", "abraranwar_usc_koch_rewind_rfm_usc_koch_rewind"]
+        "eval": ["aliangdw_metaworld_metaworld_eval", "abraranwar_usc_koch_rewind_rfm_usc_koch_rewind", "jesbu1_egodex_rfm_egodex_test"]
     },
     "quality_preference": {
         "eval": [

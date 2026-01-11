fix vqa trainer for causal lm loss

rfm/trainers/vqa_trainer.py
@@ -15,48 +15,24 @@ from rfm.data.collators.vqa import IGNORE_INDEX
 import numpy as np
 
 
-# copied because the original function forces the metric reduction
-def fixed_cross_entropy(
-    source: torch.Tensor,
-    target: torch.Tensor,
-    num_items_in_batch: torch.Tensor | None = None,
-    ignore_index: int = IGNORE_INDEX,
-    reduction: str = "mean",
-    **kwargs,
-) -> torch.Tensor:
-    loss = nn.functional.cross_entropy(source, target, ignore_index=ignore_index, reduction=reduction)
-    if reduction == "sum":
-        # just in case users pass an int for num_items_in_batch, which could be the case for custom trainer
-        if torch.is_tensor(num_items_in_batch):
-            num_items_in_batch = num_items_in_batch.to(loss.device)
-        loss = loss / num_items_in_batch
-    return loss
 
-
-def ForCausalLMLoss(
-    logits,
-    labels,
-    vocab_size: int,
-    num_items_in_batch: torch.Tensor | None = None,
-    ignore_index: int = IGNORE_INDEX,
-    shift_labels: torch.Tensor | None = None,
-    **kwargs,
-) -> torch.Tensor:
-    # Upcast to float if we need to compute the loss to avoid potential precision issues
+def ForCausalLMLoss(logits, labels, vocab_size, ignore_index=IGNORE_INDEX):
     logits = logits.float()
 
-    if shift_labels is None:
-        # Shift so that tokens < n predict n
-        labels = nn.functional.pad(labels, (0, 1), value=ignore_index)
-        shift_labels = labels[..., 1:].contiguous()
+    # logits: [B, T, V]
+    # labels: [B, T]
+    logits = logits[..., :-1, :].contiguous()
+    labels = labels[..., 1:].contiguous()
 
-    # Flatten the tokens
     logits = logits.view(-1, vocab_size)
-    shift_labels = shift_labels.view(-1)
-    # Enable model parallelism
-    shift_labels = shift_labels.to(logits.device)
-    loss = fixed_cross_entropy(logits, shift_labels, num_items_in_batch, ignore_index, **kwargs)
-    return loss
+    labels = labels.view(-1).to(logits.device)
+
+    return nn.functional.cross_entropy(
+        logits,
+        labels,
+        ignore_index=ignore_index,
+        reduction="mean",
+    )
 
 def process_progress_pred(progress):
     return progress / 100
@@ -381,7 +357,7 @@ class RFMVQATrainer(RFMHeadsTrainer):
             labels=inputs["labels"],
             vocab_size=outputs["logits"].shape[-1],
             ignore_index=IGNORE_INDEX,
-            reduction="mean",
+            #reduction="mean",
         )
         # reshape
         #loss = loss.reshape(B, -1)
0bf7e9ca8256a05f91eb106303bff49e758433f7
Jesse Zhang <jessezha@cs.washington.edu>
2026-01-13 22:28:20 +0000

    update to generate prog only data

--- scripts/generate_full_vqa_dataset.sh
+++ scripts/generate_full_vqa_dataset.sh
@@ -1,4 +1,4 @@
-# RFM-50M
+# RFM-10M
 uv run scripts/generate_vqa_dataset.py \
     --num_epochs 10 \
     --output_path vqa_datasets/rfm_train_10epochs \
@@ -6,7 +6,7 @@ uv run scripts/generate_vqa_dataset.py \
     --num_workers 36 \
     --config_overrides data.max_frames=32 "data.sample_type_ratio=[1.0, 1.0, 0.0]" "data.train_datasets=[franka,oxe,others,libero,suboptimal_fail,paired,mw]" data.min_frames_per_trajectory=1  data.dataset_type=strategy_first
 
-
+# RFM-Eval
 uv run scripts/generate_vqa_dataset.py \
     --num_epochs 0.1 \
     --output_path vqa_datasets/rfm_val_0.1epoch \
@@ -14,3 +14,20 @@ uv run scripts/generate_vqa_dataset.py \
     --num_workers 16 \
     --eval_mode \
     --config_overrides data.max_frames=32 "data.sample_type_ratio=[1.0, 1.0, 0.0]" "data.train_datasets=[franka,oxe,others,libero,suboptimal_fail,paired,mw]" data.min_frames_per_trajectory=1 data.dataset_type=strategy_first
+
+# RFM-Prog Only
+uv run scripts/generate_vqa_dataset.py \
+    --num_epochs 10.0 \
+    --output_path vqa_datasets/rfm_train_10epochs_prog_only \
+    --seed 42 \
+    --num_workers 36 \
+    --config_overrides data.max_frames=32 "data.sample_type_ratio=[0.0, 1.0, 0.0]" "data.train_datasets=[franka,oxe,others,libero,suboptimal_fail,paired,mw]" data.min_frames_per_trajectory=1  data.dataset_type=strategy_first
+
+# RFM-Eval Prog Only
+uv run scripts/generate_vqa_dataset.py \
+    --num_epochs 0.1 \
+    --output_path vqa_datasets/rfm_val_0.1epoch_prog_only \
+    --seed 42 \
+    --num_workers 16 \
+    --eval_mode \
+    --config_overrides data.max_frames=32 "data.sample_type_ratio=[0.0, 1.0, 0.0]" "data.eval_datasets=[franka,oxe,others,libero,suboptimal_fail,paired,mw]" data.min_frames_per_trajectory=1 data.dataset_type=strategy_first
--- scripts/generate_roboreward_vqa_dataset.sh
+++ scripts/generate_roboreward_vqa_dataset.sh
@@ -14,3 +14,21 @@ uv run scripts/generate_vqa_dataset.py \
     --num_workers 32 \
     --eval_mode \
     --config_overrides data.max_frames=32 "data.sample_type_ratio=[1.0, 1.0, 0.0]" "data.eval_datasets=[jesbu1_roboreward_rfm_roboreward_val]" data.min_frames_per_trajectory=1 data.dataset_type=strategy_first
+
+
+# ROBOREWARD-1M Prog Only
+uv run scripts/generate_vqa_dataset.py \
+    --num_epochs 10.0 \
+    --output_path vqa_datasets/roboreward_train_10epochs_prog_only \
+    --seed 42 \
+    --num_workers 32 \
+    --config_overrides data.max_frames=32 "data.sample_type_ratio=[0.0, 1.0, 0.0]" "data.train_datasets=[jesbu1_roboreward_rfm_roboreward_train]" data.min_frames_per_trajectory=1  data.dataset_type=strategy_first
+
+# ROBOREWARD-Eval Prog Only
+uv run scripts/generate_vqa_dataset.py \
+    --num_epochs 1.0 \
+    --output_path vqa_datasets/roboreward_val_1epoch_prog_only \
+    --seed 42 \
+    --num_workers 32 \
+    --eval_mode \
+    --config_overrides data.max_frames=32 "data.sample_type_ratio=[0.0, 1.0, 0.0]" "data.eval_datasets=[jesbu1_roboreward_rfm_roboreward_val]" data.min_frames_per_trajectory=1 data.dataset_type=strategy_first

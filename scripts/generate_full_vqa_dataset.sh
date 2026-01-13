9ec7b75f88c6873ebfb08bab3ec33848c8cf178b
Jesse Zhang <jessezha@cs.washington.edu>
2026-01-13 07:33:02 +0000

    generate full vqa datset udate

--- scripts/generate_full_vqa_dataset.sh
+++ scripts/generate_full_vqa_dataset.sh
@@ -7,3 +7,12 @@ uv run scripts/generate_vqa_dataset.py \
     --num_workers 32 \
     --config_overrides data.max_frames=32 "data.sample_type_ratio=[1.0, 1.0, 0.0]" "data.train_datasets=[franka,oxe,others,libero,suboptimal_fail,paired,mw]" data.min_frames_per_trajectory=2 
 
+
+uv run scripts/generate_vqa_dataset.py \
+    --num_samples 50_000 \
+    --save_batch_size 50_000 \
+    --output_path vqa_datasets/rfm_val_50k \
+    --seed 42 \
+    --num_workers 32 \
+    --eval_mode true \
+    --config_overrides data.max_frames=32 "data.sample_type_ratio=[1.0, 1.0, 0.0]" "data.eval_datasets=[franka,oxe,others,libero,suboptimal_fail,paired,mw]" data.min_frames_per_trajectory=2 

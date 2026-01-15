ba309c934853f19c06a18a68cdef071fe996920f
Anthony Liang <aliangdw@umich.edu>
2026-01-15 17:43:32 +0000

    update dataset category

--- rfm/data/dataset_category.py
+++ rfm/data/dataset_category.py
@@ -287,35 +287,39 @@ DATASET_MAP = {
     },
     "rfm-1m-id": {
         "eval": [
-            "abraranwar_libero_rfm_libero256_10",
-            "abraranwar_libero_rfm_libero256_object",
-            "abraranwar_libero_rfm_libero256_spatial",
-            "abraranwar_libero_rfm_libero256_goal",
-            "ykorkmaz_libero_failure_rfm_libero_10_failure",
-            "ykorkmaz_libero_failure_rfm_libero_object_failure",
-            "ykorkmaz_libero_failure_rfm_libero_spatial_failure",
-            "ykorkmaz_libero_failure_rfm_libero_goal_failure",
-            "jesbu1_roboarena_eval_debug_nowrist_roboarena_eval_debug_nowrist",
+            # "abraranwar_libero_rfm_libero256_10",
+            # "abraranwar_libero_rfm_libero256_object",
+            # "abraranwar_libero_rfm_libero256_spatial",
+            # "abraranwar_libero_rfm_libero256_goal",
+            # "ykorkmaz_libero_failure_rfm_libero_10_failure",
+            # "ykorkmaz_libero_failure_rfm_libero_object_failure",
+            # "ykorkmaz_libero_failure_rfm_libero_spatial_failure",
+            # "ykorkmaz_libero_failure_rfm_libero_goal_failure",
+
+            # "jesbu1_roboarena_eval_debug_nowrist_roboarena_eval_debug_nowrist",
+            # "jesbu1_roboarena_0825_rfm_roboarena",
+            
             "jesbu1_racer_rfm_racer_val",
-            "jesbu1_soar_rfm_soar_rfm",
-            "jesbu1_auto_eval_rfm_auto_eval_rfm",
-            "jesbu1_roboreward_rfm_roboreward_test",
+            # "jesbu1_soar_rfm_soar_rfm",
+            # "jesbu1_auto_eval_rfm_auto_eval_rfm",
+            # "jesbu1_roboreward_rfm_roboreward_test",
             "jesbu1_oxe_rfm_eval_oxe_bc_z_eval",
             "jesbu1_oxe_rfm_eval_oxe_berkeley_cable_routing_eval",
             "jesbu1_oxe_rfm_eval_oxe_bridge_v2_eval",
             "jesbu1_oxe_rfm_eval_oxe_jaco_play_eval",
             "jesbu1_oxe_rfm_eval_oxe_toto_eval",
             "jesbu1_oxe_rfm_eval_oxe_viola_eval",
-            "aliangdw_metaworld_metaworld_eval"
+            "aliangdw_metaworld_metaworld_eval",
+            "abraranwar_libero_rfm_libero256_90",
         ]
     },
-    "rfm-1m-ood_cm": {
+    "rfm-1m-ood": {
         "eval": [
-            ["jesbu1_mit_franka_p-rank_rfm_mit_franka_p-rank_rfm",
+            "jesbu1_mit_franka_p-rank_rfm_mit_franka_p-rank_rfm",
             "jesbu1_utd_so101_clean_policy_ranking_top_utd_so101_clean_policy_ranking_top",
             "aliangdw_usc_xarm_policy_ranking_usc_xarm_policy_ranking",
             "aliangdw_usc_franka_policy_ranking_usc_franka_policy_ranking",
-            "jesbu1_usc_koch_p_ranking_rfm_usc_koch_p_ranking_all"]
+            "jesbu1_usc_koch_p_ranking_rfm_usc_koch_p_ranking_all"
         ]
     }
 }
